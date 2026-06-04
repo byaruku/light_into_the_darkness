db3e704dcb7ba2798a9de4221b4519dc2c3b9da9
Bjark Hassert <stu237800@mail.uni-kiel.de>
2026-06-04 08:29:38 +0200

    Edit JoyShader
    Changed root to CanvasLayer and edit the script for it

--- light_into_the_darkness/assets/resources/joy_visible_shader.gd
+++ light_into_the_darkness/assets/resources/joy_visible_shader.gd
@@ -1,18 +1,15 @@
 class_name JoyVisibleShader
-extends Node2D
+extends CanvasLayer
 
 
 func _ready() -> void:
-	visible = false
 	add_to_group("joy_visible")
-	$CanvasLayer.visible = false
+	visible = false
 
 
 func show_for_joy():
 	visible = true
-	$CanvasLayer.visible = true
 
 
 func hide_for_joy():
 	visible = false
-	$CanvasLayer.visible = false
