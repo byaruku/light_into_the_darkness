b22bc8a99f29190394296175ae70e20640426053
Bjark Hassert <stu237800@mail.uni-kiel.de>
2026-07-21 16:19:13 +0200

    Bug fix and adjustment
    Quizdialog lines never set back
    Enddialog got line as character name because it ends on :
    Townportal to mid of area

--- light_into_the_darkness/assets/characters/npc_memory_ichika.gd
+++ light_into_the_darkness/assets/characters/npc_memory_ichika.gd
@@ -57,6 +57,7 @@ func interact() -> void:
 		
 		DialogueManager.start_dialogue(yarn_node)
 		await DialogueManager.on_dialog_finished
+	
 	DialogueManager.switch_option_texture(true)
 	DialogueManager.start_dialogue(yarn_node_after_quiz)
 	await DialogueManager.on_dialog_finished
